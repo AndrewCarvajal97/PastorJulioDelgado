dfd96e3dcdd9cbc6de1cf92994c4abc58dde61a7
andrew23 <cosmopablocarvajal@gmail.com>
2024-07-24 19:20:52 -0500

    Updates

---
+++ Data.js
@@ -0,0 +1,322 @@
+export const Logo ={
+    ruta:"./img/imagenLogoPatorJulioTransparente.png",
+    imagen:"imagenLogoPatorJulioTransparente",
+    alt:"Logo de la compañia actualmente"
+}
+export const Footer={
+    informacion:"Empresa distribuidora y comercializadora de productos de consumo masivo.",
+    telefono:"6076371018",
+    correo:"contactenos@dpjd.com"
+}
+
+export const CarruselHome=[
+    {
+        ruta:"./img/Inicio/CarruselSuperior/imagenCarruselSuperior-0.jpg",
+        alt:"Imagen principal del carrusel superior",
+
+    },
+    {
+        ruta:"./img/Inicio/CarruselSuperior/imagenCarruselSuperior-1.jpg",
+        alt:"Imagen segundaria del carrusel superior",
+    }
+]
+
+export const SlidersHome= [
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-1.png",
+      alt:"Imagen principal del slider Inicio",
+    },
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-2.png",
+      alt:"Imagen secundaria del slider Inicio",
+    },
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-3.png",
+      alt:"Imagen tercera del slider Inicio",
+    },
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-4.png",
+      alt:"Imagen cuarta del slider Inicio",
+    },
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-5.png",
+      alt:"Imagen quinta del slider Inicio",
+    },
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-6.png",
+      alt:"Imagen sexta del slider Inicio",
+    },
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-7.png",
+      alt:"Imagen séptima del slider Inicio",
+    },
+    {
+      ruta:"./img/Inicio/Slider/SliderHome-8.png",
+      alt:"Imagen octava del slider Inicio",
+    }
+  ];
+export const PagosHome='www.zonapagos.com/t_ecdistribucionespastorjuliodelgadosa';
+
+export const PrimerBannerHome={
+  imagen:'./img/Inicio/Banners/imagenBannerHomeIntermedio.png',
+  alt:'Imagen del banner intermedio Inicio',
+}
+
+export const dataTarjetas= [
+  {
+    imagen: './img/Inicio/Tarjetas/imagenTargetaCamion.png',
+    alt: 'Cobertura',
+    titulo: 'Cobertura',
+    descripcion: 'Contamos con un gran alcance de 252 municipios a nivel nacional.'
+  },
+  {
+    imagen: './img/Inicio/Tarjetas/imagenTargetaEntregas.png',
+    alt: 'Distribución',
+    titulo: 'Distribución',
+    descripcion: 'Actualmente tenemos 5 centros de distribución ubicados estratégicamente alrededor del país.'
+  },
+  {
+    imagen: './img/Inicio/Tarjetas/imagenTargetaPaquetes.png',
+    alt: 'Clientes',
+    titulo: 'Clientes',
+    descripcion: 'Hemos impactado alrededor de 22,000 empresas naturales y jurídicas.'
+  }
+]
+
+export const dataMapa=[
+  { name: 'Aguachica', address: 'Calle 5 No. 37 - 40 Barrio Maria Eugenia', phone: '3174287168', top: '25%', left: '51%', imagen: './img/Inicio/Mapa/marcador.png', imagenAzul: './img/Inicio/Mapa/marcador-azul.png' },
+  { name: 'Bucaramanga', address: 'Calle 70 No. 44W - 150 Vía Girón', phone: '(607) 6371018', top: '52%', left: '58%', imagen: './img/Inicio/Mapa/marcador.png', imagenAzul: './img/Inicio/Mapa/marcador-azul.png' },
+  { name: 'Cucuta', address: 'N.S. Avda. 6 No.8N-70 zona industrial vía aeropuerto zona industrial', phone: '(607) 5780670', top: '41%', left: '64%', imagen: './img/Inicio/Mapa/marcador.png', imagenAzul: './img/Inicio/Mapa/marcador-azul.png' },
+  { name: 'Duitama', address: 'Calle 3 No. 3-Manzana 1 bodega 1 Ciudadela industrial', phone: '(608) 7638012', top: '69%', left: '58%', imagen: './img/Inicio/Mapa/marcador.png', imagenAzul: './img/Inicio/Mapa/marcador-azul.png' },
+  { name: 'Valledupar', address: 'Manzana 29-30 vía aeropuerto zona industrial', phone: '(605) 5820404', top: '5%', left: '56%', imagen: './img/Inicio/Mapa/marcador.png', imagenAzul: './img/Inicio/Mapa/marcador-azul.png' },
+]
+
+export const SegundoBannerHome={
+  imagen:'./img/Inicio/Banners/imagenBannerHomeInferior.jpg',
+  alt:'Imagen del banner intermedio Inicio',
+}
+
+
+export const BannersAnimadosTexto=[
+  {
+    title:"Misión",
+    subtitle:"Empresarial",
+    paragraphStart:"Atender de manera eficiente y efectiva las",
+    highlightText:" necesidades del mercado",
+    paragraphEnd:"de consumo popular, manejado de una forma honesta, profesional y cumplida.",
+    backgroundImageUrl:"./img/NuestraTrayectoria/BannerTextoAnimado/Mision.76aa83079ed870bb61fe.jpg",
+    direction:"left",
+    titleColor:"white",
+    subtitleColor:"white",
+    paragraphColor:"#F8F8F8",
+    highlightColor:"white",
+    titleAlign:"left",
+    subtitleAlign:"left",
+    paragraphAlign:"left",
+  },
+  {
+    title:"Visión",
+    subtitle:"Proyectada",
+    backgroundImageUrl:"./img/NuestraTrayectoria/BannerTextoAnimado/Vision.a3ce5e71df2a864884a3.jpg",
+    paragraphStart:"Posicionamiento de nuestra empresa como",
+    highlightText:" líder en el mercado de productos de consumo popular en el Oriente Colombiano ",
+    paragraphEnd:"e incursionar a mediano plazo en el mercado nacional. Junto a la implementación de avances tecnológicos, buscando siempre la calidad y eficiencia de nuestros servicios y productos.",
+    direction:"right",
+    titleColor:"blue",
+    subtitleColor:"blue",
+    paragraphColor:"blue",
+    highlightColor:"blue-hard",
+    titleAlign:"center",
+    subtitleAlign:"center",
+    paragraphAlign:"right",
+  }
+
+]
+
+export const ValoresCoorporativos=[
+  {
+    title: 'Honestidad',
+    description: 'Actuar con veracidad, honradez, rectitud e integridad siempre.'
+  },
+  {
+    title: 'Responsabilidad',
+    description: 'Hacer buen uso de nuestra libertad para hacer las cosas bien, aún cuando no nos están viendo.'
+  },
+  {
+    title: 'Servicio',
+    description: 'Escuchar, investigar y satisfacer las necesidades de nuestros clientes en forma oportuna y veraz.'
+  },
+  {
+    title: 'Sentido de pertenencia',
+    description: 'Sentir la empresa como propia y formar parte integral de ella cuidarla, ayudarla, amarla, protegerla, asegurando su crecimiento y permanencia en el mercado.'
+  }
+]
+
+export const dataCarruselHistoria=[
+  {
+    year: '1934',
+    title: 'El nacimiento',
+    span1: 'Don Pastor Julio Delgado',
+    description: ', nació el 10 de abril de 1.934 en Girón, Santander, hijo de dos incansables trabajadores que le dieron ejemplo para salir adelante en su vida personal y profesional.',
+    Parrafo2: 'Trabajador desde pequeño, Don Pastor creció en una familia unida y emprendedora: es el cuarto de once hermanos, la mayoría involucrados en el negocio de consumo masivo.',
+    image: './img/NuestraTrayectoria/CarrouselHistoria/imagenInicios.png',
+  },
+  {
+    year: '1973',
+    title: 'Gran Cacharreria Oriental',
+    description: 'Su gran visión y deseo de superación lo llevó a establecer su primera sociedad con el señor Luis Francisco Mejía en la compra del “GRANERO ORIENTAL”.',
+    Parrafo2: 'El cual se constituyó en la base y punto clave para el futuro desarrollo empresarial de “DISTRIBUCIONES PASTOR JULIO DELGADO”',
+    image: './img/NuestraTrayectoria/CarrouselHistoria/imagenGraneroOriental.png',
+  },
+  {
+    year: '1976',
+    title: 'Sede Cúcuta',
+    description: 'Se dio inicio en la Calle 9A N 1-75 con el Señor Héctor Castellanos como Gerente, con 3 vendedores; uno de periferia, un encargado de los negocios de la Sexta y Don Oscar Tomas quien se desempeñaba como vendedor de poblaciones, en bodega se contaba con 3 empleados y 2 en el área administrativa.',
+    image: './img/NuestraTrayectoria/CarrouselHistoria/imagenSedeCucuta.png',
+  },
+  {
+    year: '2005',
+    title: 'Sede Valledupar',
+    span1: '',
+    description: 'Se dio inicio en la Manzana 26 – 30Via aeropuerto con el sr. Jorge Delgado como Gerente, con 5 vendedores, en bodega se contaba con 2 empleados y 2 en el área administrativa.',
+    Parrafo2: '',
+    image: './img/NuestraTrayectoria/CarrouselHistoria/imagenSedeValledupar.png',
+  },
+  {
+    year: '2007',
+    title: 'Sede Duitama',
+    span1: '',
+    description: 'Se dio inicio en la Manzana bodega 1 Calle 3 Nº 3-05 Ciudadela parque Industrial con el Señor Eliseo Remolina como Gerente, con 6 vendedores, en bodega se contaba con 3 empleados y 2 en el área administrativa.',
+    Parrafo2: '',
+    image: './img/NuestraTrayectoria/CarrouselHistoria/imagenSedeDuitama.png',
+  },
+]
+
+export const PrimerBannerMarcas={
+  imagen:'./img/NuestrasMarcas/imagenBannerMarcas.jpg',
+  alt:'Imagen del banner intermedio Inicio',
+  titulo: 'Nuestras marcas',
+  subtitulo: 'Proveedoras'
+}
+
+export const Categorias=[
+  {
+    name: 'Alimentos', 
+    icon: './img/NuestrasMarcas/IconosCategorias/imagenCategoriaAlimentosSelectora.png', 
+    images: [
+        './img/NuestrasMarcas/SlidersCategorias/Alimentos/imagenSliderCategoriaAlimentos.png',
+        './img/NuestrasMarcas/SlidersCategorias/Alimentos/imagenSliderCategoriaAlimentos1.png',
+        './img/NuestrasMarcas/SlidersCategorias/Alimentos/imagenSliderCategoriaAlimentos2.png',
+        './img/NuestrasMarcas/SlidersCategorias/Alimentos/imagenSliderCategoriaAlimentos3.png',
+        './img/NuestrasMarcas/SlidersCategorias/Alimentos/imagenSliderCategoriaAlimentos4.png',
+        './img/NuestrasMarcas/SlidersCategorias/Alimentos/imagenSliderCategoriaAlimentos5.png']
+  } ,
+  {
+    name: 'Cuidado Del Hogar', 
+    icon: './img/NuestrasMarcas/IconosCategorias/imagenCategoriaCuidadoHogarSelectora.png', 
+    images: [
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoHogar/imagenSliderCategoriaCuidadoHogar1.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoHogar/imagenSliderCategoriaCuidadoHogar1.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoHogar/imagenSliderCategoriaCuidadoHogar2.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoHogar/imagenSliderCategoriaCuidadoHogar3.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoHogar/imagenSliderCategoriaCuidadoHogar4.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoHogar/imagenSliderCategoriaCuidadoHogar5.png',
+        ]
+  },
+  {
+    name: 'Cuidado Personal', 
+    icon: './img/NuestrasMarcas/IconosCategorias/imagenCategoriaCuidadoPersonalSelectora.png', 
+    images: [
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoPersonal/imagenSliderCategoriaCuidadoPersonal.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoPersonal/imagenSliderCategoriaCuidadoPersonal1.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoPersonal/imagenSliderCategoriaCuidadoPersonal2.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoPersonal/imagenSliderCategoriaCuidadoPersonal3.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoPersonal/imagenSliderCategoriaCuidadoPersonal4.png',
+        './img/NuestrasMarcas/SlidersCategorias/CuidadoPersonal/imagenSliderCategoriaCuidadoPersonal5.png'
+        ]
+  },
+  {
+    name: 'Farmacia', 
+    icon: './img/NuestrasMarcas/IconosCategorias/imagenCategoriaFarmaciaSelectora.png', 
+    images: [
+        './img/NuestrasMarcas/SlidersCategorias/Farmacia/imagenSliderCategoriaFarmacia.png',
+        './img/NuestrasMarcas/SlidersCategorias/Farmacia/imagenSliderCategoriaFarmacia1.png',
+        './img/NuestrasMarcas/SlidersCategorias/Farmacia/imagenSliderCategoriaFarmacia2.png',
+        './img/NuestrasMarcas/SlidersCategorias/Farmacia/imagenSliderCategoriaFarmacia3.png',
+        './img/NuestrasMarcas/SlidersCategorias/Farmacia/imagenSliderCategoriaFarmacia4.png',
+        './img/NuestrasMarcas/SlidersCategorias/Farmacia/imagenSliderCategoriaFarmacia5.png'
+        ]
+  },
+  {
+    name: 'Mascotas', 
+    icon: './img/NuestrasMarcas/IconosCategorias/imagenCategoriaMascotasSelectora.png', 
+    images: [
+        './img/NuestrasMarcas/SlidersCategorias/Mascotas/imagenSliderCategoriaMascotas.png',
+        './img/NuestrasMarcas/SlidersCategorias/Mascotas/imagenSliderCategoriaMascotas1.png',
+        ]
+  },
+  {
+    name: 'Papeleria', 
+    icon: './img/NuestrasMarcas/IconosCategorias/imagenCategoriaPapeleriaSelectora.png', 
+    images: [
+        './img/NuestrasMarcas/SlidersCategorias/Papeleria/imagenSliderCategoriaPapeleria.png',
+        './img/NuestrasMarcas/SlidersCategorias/Papeleria/imagenSliderCategoriaPapeleria1.png',
+        ]
+  },
+  
+  
+]
+
+export const PrimerBannerTrabajaConNosotros={
+  imagen:'./img/TrabajaConNosotros/imagenBannerTrabajaConNosotros.jpg',
+  alt:'Imagen del banner intermedio trabaja con nosotros',
+  titulo:'Trabaja con',
+  subtitulo:'nosotros'
+}
+
+export const textos=[
+  {
+    parrafo:'En',
+    strong:'Distribuciones Pastor Julio Delgado',
+    parrafo2:'creemos en el potencial de nuestros colaboradores, quienes permanecen en constante crecimiento personal y profesional integrando los valores corporativos y el trabajo en equipo.'
+  },
+
+  {
+    parrafo:'Contamos con más de',
+    strong:'500 empleados a nivel nacional',
+    parrafo2:'que desde su experticia hacen que cada unidad de trabajo permanezca unida bajo un mismo objetivo: Mejoramiento continuo.'
+  }
+]
+
+export const primerBannerPoliticas={
+  imagen:'./img/Politicas/imagenBannerPoliticas.jpg',
+  alt:'Imagen del banner intermedio politicas',
+  titulo:'Políticas',
+  subtitulo:'Empresariales'
+}
+export const SegundoBannerTrabajaConNosotros={
+  titulo:'Súmate a nuestra gran',
+  subtitulo:'Familia'
+}
+
+export const OfertasAlPublico={
+    imagen:'./img/TrabajaConNosotros/OfertasLaborales.jpg',  
+}
+
+export const CarruselOfertasDeEmpleo=[
+  {
+      Cargo: "Director Comercial"
+  },
+  {
+      Cargo: "Supervisor"
+  },
+  {
+      Cargo: "Vendedor"
+  },
+  {
+      Cargo: "Cliente"
+  },
+]
+
+
+
